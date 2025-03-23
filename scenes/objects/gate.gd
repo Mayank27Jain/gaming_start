e39b060a8ee23826ce5453bdbb64c10a20e19e6a
Mayank Jain <mayank.anushaktinagar@gmail.com>
2025-03-23 17:25:20 +0800

    Added some basic physics to TileSet(s)

--- scenes/objects/gate.gd
+++ scenes/objects/gate.gd
@@ -5,5 +5,5 @@ signal player_entered_gate(body)
 func _on_entrance_body_entered(body: Node2D) -> void:
 	player_entered_gate.emit(body)
 
-func _on_entrance_body_exited(body: Node2D) -> void:
+func _on_entrance_body_exited(_body: Node2D) -> void:
 	print("player exited??")
